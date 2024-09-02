af9034d4ea8a1dc34a7a78656b09941a05f70ac5
Momen1995 <momensrk@gmail.com>
2024-09-02 20:39:49 +0600

    display movement done

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ function displayMovements(userData){
   const moveMent = userData.movements;
   
   moveMent.forEach((move,i) => {
+    const type = move > 0 ? "deposit" : "withdraw";
+
     const html = `
-    <div class="movement-row bg-[#fff] flex justify-between w-full  p-4 rounded border-b-2 border-b-gray-200">
-        <h3 class="text-gray-700 font-semibold">${i+1} Deposit</h3>
-        <p class="text-gray-700 font-semibold">Date: 10/07/2024</p>
-        <p class="text-gray-700 font-semibold">${move}</p>
+    <div class="movement-row bg-[#fff] flex justify-between w-full p-4 rounded text-[15px]">
+        <h3 class="font-semibold balance-${type} px-2 py-1 rounded">${i+1}  ${type}</h3>
+        <p class="text-gray-600 font-semibold">Date: 10/07/2024</p>
+        <p class="text-gray-600 font-semibold">${move}</p>
     </div>
     `;
 
