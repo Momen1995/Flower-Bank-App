ee37a9faa0ee4b1dd1a47c9c44b4b16048a69fc1
Momen1995 <momensrk@gmail.com>
2024-09-02 21:07:18 +0600

    money calculate done

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import userData from "./data.js";
 
 const movementContainer = document.querySelector(".movement-container");
+const moneyIn = document.querySelector(".money-in");
+const moneyInterest = document.querySelector(".money-interest");
+const moneyOut = document.querySelector(".money-out")
 
 //display movements
 function displayMovements(userData){
@@ -22,3 +25,20 @@ function displayMovements(userData){
 }
 
 displayMovements(userData[0])
+
+//balance calculate
+function balanceCalc(userData){
+  const moneyAdd = userData.movements.filter(move => move > 0).reduce((acc,deposit) => acc + deposit,0);
+  moneyIn.textContent = `${moneyAdd} TK`;
+
+  const moneyInterstAcc = userData.movements
+    .filter((move) => move > 0)
+    .map((deposit) => (deposit * userData.interestRate) / 100).reduce((acc,interest) => acc + interest,0);
+  moneyInterest.textContent = `${moneyInterstAcc} TK`
+
+  const moneyOutFromAccount = userData.movements
+    .filter((move) => move < 0)
+    .reduce((acc, deposit) => acc + deposit, 0);
+  moneyOut.textContent = `${Math.abs(moneyOutFromAccount)}`;
+}
+balanceCalc(userData[0])
