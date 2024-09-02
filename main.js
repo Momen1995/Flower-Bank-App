d36866c5b3f6ecede5ecf7a702082c932687d7ea
Momen1995 <momensrk@gmail.com>
2024-09-02 21:59:58 +0600

    total balance add

--- main.js
+++ main.js
@@ -3,42 +3,57 @@ import userData from "./data.js";
 const movementContainer = document.querySelector(".movement-container");
 const moneyIn = document.querySelector(".money-in");
 const moneyInterest = document.querySelector(".money-interest");
-const moneyOut = document.querySelector(".money-out")
+const moneyOut = document.querySelector(".money-out");
+const totalMoney = document.querySelector(".total-balance");
 
+//log in implement
 //display movements
-function displayMovements(userData){
+function displayMovements(userData) {
   const moveMent = userData.movements;
-  
-  moveMent.forEach((move,i) => {
+
+  moveMent.forEach((move, i) => {
     const type = move > 0 ? "deposit" : "withdraw";
 
     const html = `
     <div class="movement-row bg-[#fff] flex justify-between w-full p-4 rounded text-[15px]">
-        <h3 class="font-semibold balance-${type} px-2 py-1 rounded">${i+1}  ${type}</h3>
+        <h3 class="font-semibold balance-${type} px-2 py-1 rounded">${
+      i + 1
+    }  ${type}</h3>
         <p class="text-gray-600 font-semibold">Date: 10/07/2024</p>
         <p class="text-gray-600 font-semibold">${move}</p>
     </div>
     `;
 
-    movementContainer.insertAdjacentHTML("beforebegin",html)
-  })
+    movementContainer.insertAdjacentHTML("beforebegin", html);
+  });
 }
 
-displayMovements(userData[0])
+displayMovements(userData[0]);
 
 //balance calculate
-function balanceCalc(userData){
-  const moneyAdd = userData.movements.filter(move => move > 0).reduce((acc,deposit) => acc + deposit,0);
+function balanceCalc(userData) {
+  const moneyAdd = userData.movements
+    .filter((move) => move > 0)
+    .reduce((acc, deposit) => acc + deposit, 0);
   moneyIn.textContent = `${moneyAdd} TK`;
 
   const moneyInterstAcc = userData.movements
     .filter((move) => move > 0)
-    .map((deposit) => (deposit * userData.interestRate) / 100).reduce((acc,interest) => acc + interest,0);
-  moneyInterest.textContent = `${moneyInterstAcc} TK`
+    .map((deposit) => (deposit * userData.interestRate) / 100)
+    .reduce((acc, interest) => acc + interest, 0);
+  moneyInterest.textContent = `${moneyInterstAcc} TK`;
 
   const moneyOutFromAccount = userData.movements
     .filter((move) => move < 0)
     .reduce((acc, deposit) => acc + deposit, 0);
-  moneyOut.textContent = `${Math.abs(moneyOutFromAccount)}`;
+  moneyOut.textContent = `${Math.abs(moneyOutFromAccount)} Tk`;
 }
-balanceCalc(userData[0])
+
+balanceCalc(userData[0]);
+
+function totalBalance(userData) {
+  const balance = userData.movements.reduce((acc, value) => acc + value, 0);
+  totalMoney.textContent = `${balance} Tk`;
+}
+
+totalBalance(userData[0]);
